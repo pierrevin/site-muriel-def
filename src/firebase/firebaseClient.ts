d21bb3c92510f391023f6d83e3bc23cbee62236a
Pierre Vincenot <pierrevincenot@immediatlab.fr>
2025-07-18 14:44:29 +0000

    🧨 Tu appelles signInWithPassword sans passer par auth du client Firebas

--- src/firebase/firebaseClient.ts
+++ src/firebase/firebaseClient.ts
@@ -2,15 +2,16 @@
 // côté client (dans le navigateur).
 
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 import firebaseConfig from "./firebaseConfig";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 // Initialise Firebase de manière sécurisée (idempotente).
 // Cela empêche l'initialisation de se produire plusieurs fois.
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// Exporte les services Firebase dont l'application a besoin.
+// Exporte uniquement l'instance de l'application.
+// Les services (auth, storage) seront obtenus à la demande dans les composants.
 const auth = getAuth(app);
 const storage = getStorage(app);
 
