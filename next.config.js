aabee66939aada57ea0a091b98dcd4300ebf313e
Pierre Vincenot <pierrevincenot@immediatlab.fr>
2025-07-18 15:52:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
