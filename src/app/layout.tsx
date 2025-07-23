0c5eca2c75a3227870b673097e312315b130e77c
Pierre Vincenot <pierrevincenot@immediatlab.fr>
2025-07-23 04:39:27 +0000

    Rajoute le logo en favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,10 +38,14 @@ export default function RootLayout({
   // La prop heroImageUrl est maintenant passée depuis la page.
   // C'est une astuce pour permettre à une page enfant de modifier le <head> du layout parent.
   const { heroImageUrl } = params;
+  const logoUrl = "https://firebasestorage.googleapis.com/v0/b/les-trucs-de-mumu-g9rzm.firebasestorage.app/o/uploads%2FJmt1vFlq3UeVzjvpiTwBJ56dWu93%2F1752737122228-Logo_LTDM_V2%20jaune.png?alt=media&token=52cf14b4-f786-4f90-a790-39458b13f8ff";
 
   return (
     <html lang="fr" className={`${playfair.variable} ${pt_sans.variable}`}>
       <head>
+        <link rel="icon" href={logoUrl} type="image/png" />
+        <link rel="shortcut icon" href={logoUrl} type="image/png" />
+        <link rel="apple-touch-icon" href={logoUrl} />
         {heroImageUrl && (
           <link
             rel="preload"
