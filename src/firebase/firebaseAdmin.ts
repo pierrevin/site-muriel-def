9ff701af3572cc810b6c1c33699393692bd0742c
Pierre Vincenot <pierrevincenot@immediatlab.fr>
2025-07-24 10:48:47 +0000

    page-44a0274b3951f3b9.js:1  POST https://lestrucsdemumu.fr/api/content

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -1,5 +1,6 @@
 // src/firebase/firebaseAdmin.ts
 import admin from 'firebase-admin';
+import firebaseConfig from './firebaseConfig';
 
 // Ce fichier est destiné à une exécution côté serveur uniquement.
 
@@ -8,7 +9,10 @@ import admin from 'firebase-admin';
 // Le SDK essaiera de trouver les credentials automatiquement via les "Application Default Credentials".
 if (!admin.apps.length) {
   try {
-    admin.initializeApp();
+    // Utiliser le projectId de la configuration client garantit que le serveur sait à quel projet se connecter.
+    admin.initializeApp({
+        projectId: firebaseConfig.projectId
+    });
   } catch (e) {
      console.error('Firebase admin initialization error', e);
   }
